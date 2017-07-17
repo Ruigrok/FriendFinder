6ca6505d1c269608584342b532a45f7bc38f9ce4
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2017-07-17 12:53:04 -0500

    adding main files and

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var nodemon = require("nodemon");
+var bulma = require("bulma");
