f36aff559d3e9151935ab5743a3a89fad32b2642
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2017-07-19 13:06:25 -0500

    updated site flow and display

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsArr = [
+    {
+        name: 'Hodor',
+        photo: 'http://digitalspyuk.cdnds.net/16/16/980x490/landscape-1461341810-749724-1563879-4256x2832.jpg',
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ]
+    },
+    {
+        name: 'Sandor Clegane',
+        photo: 'https://vignette3.wikia.nocookie.net/villains/images/4/47/The_Hound.jpg/revision/latest?cb=20160215075509',
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+        ]
+    },
+    {
+        name: 'Tyrion Lannister',
+        photo: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/6/61/The_children_Tyrion_with_Bow_S4.png/revision/latest?cb=20140616190514',
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: 'Samwell Tarly',
+        photo: 'http://www1.pictures.zimbio.com/mp/5trX1nvWZBxl.jpg',
+        scores: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: 'Daenerys Targaryen',
+        photo: 'https://pbs.twimg.com/profile_images/749378421759827968/-CqzLLU3.jpg',
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+];
+
+module.exports = friendsArr;
--- server.js
+++ server.js
@@ -4,4 +4,25 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var nodemon = require("nodemon");
-var bulma = require("bulma");
+//var bulma = require("bulma");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing (json)
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
